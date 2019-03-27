b655cea778844d2b39cd69e7e91890a6883b7279
Lee Changseok <leechangseok@Lees-MBP.fc.ca>
2019-03-27 13:19:54 -0400

    basic chat app build complete

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-// Initialize an express app and set it up
-const express = require('express');
-const app = express();
-const io = require('socket.io');
+var express = require('express');
+var app = express();
 
-// some config stuff
+var io = require('socket.io')();
+
 const port = process.env.PORT || 3000;
 
-// tell our app to use the public folder for static files
+// tell express where our static files are
 app.use(express.static('public'));
 
-//instatnt 
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+
+  res.sendfile(__dirname + '/views/index.html');
+
 });
 
-// create server variable 
-const server = app.listen(port, () => {
-    console.log(`app is running on the port ${port}`);
+// const server = http.listen(port, () => {
+//   console.log(`app is running on port ${port}`);
+// });
+
+const server = app.listen(port, ()=> {
+  console.log(`app is running on port ${port}`);
+});
+
+//! socket.io chat app stuff to follow note. socket is client
+
+io.attach(server);
+
+io.on('connection', function (socket) {
+  console.log('a user has connected');
+
+  // sending back msg to connecting tool
+  socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'});
+
+  // listen for incoming message form anyone connected to the app
+  socket.on('chat message', function(msg) {
+    console.log('message', msg, 'socket', socket.id);
+
+    // send message to everyone connected to the app
+    io.emit('chat message', { id: `${socket.id}`, message: msg });
+  });
+
+  socket.on('disconnect', function(){
+    console.log('a user has disconnected');
+  });
 });
